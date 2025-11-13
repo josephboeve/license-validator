2110f542ebdd7975a769d02a25fd94c6a12b1214
josephboeve <josephboeve@yahoo.com>
2025-11-13 13:53:38 -0800

    Update generate.py

--- api/generate.py
+++ api/generate.py
@@ -1,1 +1,19 @@
+from http.server import BaseHTTPRequestHandler
+import json
+import secrets
+import os
+import redis
 
+kv = redis.from_url(os.environ.get('KV_URL'))
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+        new_key = secrets.token_hex(16)
+        kv.set(new_key, 'active')
+        response = {'key': new_key}
+        
+        self.wfile.write(json.dumps(response).encode())
